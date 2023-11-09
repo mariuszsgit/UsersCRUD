238d21a01172c2c9db166bf48698b4acb3bde28c
Mariusz Ściseł <mariuszs@onet.pl>
2023-11-09 22:51:34 +0100

    Project initial commit with template

---
+++ src/main/java/pl/coderslab/Main.java
@@ -0,0 +1,14 @@
+package pl.coderslab;
+
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Connection conn = DbUtil.getConnection();
+
+    }
+}
